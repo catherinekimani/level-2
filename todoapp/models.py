d66fa4bb92f604f4445adfae73b69b5bd33459c0
catherinekimani <catherinekimani882@gmail.com>
2022-09-06 17:35:13 +0300

    Ft: Create Todolist class & register it on admin.py

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class TodoList(models.Model):
+    PRIORITY_CHOICES = (
+        ('High','High'),
+        ('Medium','Medium'),
+        ('Low','Low'),
+    )
+    task_name = models.CharField(max_length=255)
+    priority= models.CharField(choices=PRIORITY_CHOICES,max_length=100)
+    
+    
+    def __str__(self):
+        return self.task_name
+    
