e1fd12330382faf0ed65db410f5affd27261c1e2
catherinekimani <catherinekimani882@gmail.com>
2022-09-07 22:05:17 +0300

    Ft: Create home view function

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import TodoList
 # Create your views here.
+def home(request):
+    tasks = TodoList.objects.all()
+    
+    if request.method == 'POST':
+        new_task = request.POST.get('new_task')
+        priority = request.POST.get('priority')
+        task = TodoList(task_name=new_task,priority=priority)
+        task.save()
+    return render(request,'index.html',{'tasks':tasks}) 
+
