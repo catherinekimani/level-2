93c73c773f6423fd5911024ba4fee810e12040e3
catherinekimani <catherinekimani882@gmail.com>
2022-09-19 18:40:00 +0100

    Create profile class

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,6 +1,18 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_bio = models.TextField(max_length=150)
+    user_contact = models.EmailField(max_length=100)
+    user_profile = CloudinaryField('image')
+    location = models.CharField(max_length=100,blank=True,null=True)
+    
+    def __str__(self):
+        return f'{self.user} Profile'
+
+
 class TodoList(models.Model):
     PRIORITY_CHOICES = (
         ('High','High'),
